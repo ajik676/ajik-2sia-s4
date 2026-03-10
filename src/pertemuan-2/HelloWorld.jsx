6ada1fd93cce8593895661fca08eeaff985122a9
guest-persil <jihan24si@mahasiswa.pcr.ac.id>
2026-03-10 09:17:57 +0700

    pertemuan2

---
+++ src/pertemuan-2/HelloWorld.jsx
@@ -0,0 +1,53 @@
+export default function HelloWorld(){
+        const propsUserCard = {
+        nama: "Goku",
+        nim: "999999",
+        tanggal: "2025-01-01"
+    }
+
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>Selamat Belajar ReactJs</p>
+<img src="./img/gambar2.jpg" alt="beruang" width={190}/>
+                <UserCard 
+	            nama="Ajik" 
+	            nim="2457301091"
+	            tanggal={new Date().toLocaleDateString()}
+	          />
+             <UserCard {...propsUserCard}/>
+            <GreetingsBinjai/>
+            <QuoteText/>
+        </div>
+    )
+}
+
+
+function QuoteText() {
+    const text = "Mulutmu Harimaumu";
+    const text2 = "Aku ingin jadi macan";
+    return (
+        <div>
+            <hr/>
+            <p>{text.toLowerCase()}</p>
+            <p>{text2.toUpperCase()}</p>
+        </div>
+    )
+}
+
+function GreetingsBinjai(){
+    return(
+        <small>Salam dari binjai</small>
+    )
+    }
+
+   function UserCard(props){
+    return (
+        <div>
+            <hr/>
+            <h3>Nama: {props.nama}</h3>
+            <p>NIM: {props.nim}</p>
+            <p>Tanggal: {props.tanggal}</p>
+        </div>
+    )
+} 
